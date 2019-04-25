5786b1993249e23600b2e319e1c508ff7f7c46d5
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-25 13:11:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,10 +9,11 @@ class Artist
     @songs = []
   end
 
-  def add_song(song)
+def add_song(song)
     @songs << song
     song.artist = self
   end
+ 
 
   def add_song_by_name(name)
     song = Song.new(name)
